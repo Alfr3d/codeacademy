db2e6cf0abcf0696495e3caddc4f1692864ef93c
Alfr3d <alfred@Alfreds-MBP.lan>
2022-12-08 21:40:18 +0200

    Data Processor ex Part 1

---
+++ Data_Processor_prod/src/Service/Encoder/XmlEncoder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Processor\Service\Encoder;
+
+class XmlEncoder implements EncoderInterface
+{
+
+    public function encode(array $data): string
+    {
+        // TODO: Implement encode() method.
+    }
+}
---
+++ Data_Processor_prod/src/Service/OutputHandler/FileOutputHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Processor\Service\OutputHandler;
+
+class FileOutputHandler implements OutputHandlerInterface
+{
+    public function handle(string $encodedData): void
+    {
+        // TODO: Implement handle() method.
+    }
+}
