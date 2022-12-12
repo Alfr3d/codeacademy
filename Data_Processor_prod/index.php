9bc31362c1d9b230791a67f59efcb5cbf3253f19
Alfr3d <alfred@Alfreds-MBP.lan>
2022-12-12 21:02:17 +0200

    added composer autoloader

--- Data_Processor_prod/index.php
+++ Data_Processor_prod/index.php
@@ -2,44 +2,7 @@
 
 declare(strict_types=1);
 
-
-spl_autoload_register(function ($className) {
-    if ($className === 'Processor\App') {
-        require './src/App.php';
-    }
-
-    if ($className === 'Processor\Service\CategoryService') {
-        require './src/Service/CategoryService.php';
-    }
-
-    if ($className === 'Processor\Service\DataProcessorService') {
-        require './src/Service/DataProcessorService.php';
-    }
-
-    if ($className === 'Processor\Service\Encoder\EncoderInterface') {
-        require './src/Service/Encoder/EncoderInterface.php';
-    }
-
-    if ($className === 'Processor\Service\Encoder\JsonEncoder') {
-        require './src/Service/Encoder/JsonEncoder.php';
-    }
-
-    if ($className === 'Processor\Service\Encoder\XmlEncoder') {
-        require './src/Service/Encoder/XmlEncoder.php';
-    }
-
-    if ($className === 'Processor\Service\OutputHandler\OutputHandlerInterface') {
-        require './src/Service/OutputHandler/OutputHandlerInterface.php';
-    }
-
-    if ($className === 'Processor\Service\OutputHandler\FileOutputHandler') {
-        require './src/Service/OutputHandler/FileOutputHandler.php';
-    }
-
-    if ($className === 'Processor\Service\OutputHandler\TerminalOutputHandler') {
-        require './src/Service/OutputHandler/TerminalOutputHandler.php';
-    }
-});
+require_once './vendor/autoload.php';
 
 $appObj = new Processor\App();
 $appObj->execute();
